dc57cd0224e2e27577d6e635b690ae01bbbffe7e
dbracho12 <danny.bracho2@gmail.com>
2024-03-09 02:44:59 +0000

    Se realizo el ejemplo_.md

--- ejemplos_clase/ejemplo_1_gps_mock.py
+++ ejemplos_clase/ejemplo_1_gps_mock.py
@@ -8,7 +8,11 @@ config = dotenv_values()
 
 # ----------------------
 # Aquí crear los callbacks de MQTT
-
+def on_connect(client, userdata, flags,rc):
+    if rc == 0:
+        print("Me conecte")
+    else:
+        print("Fallo la conexion") 
 
 # ----------------------
 
@@ -18,7 +22,10 @@ if __name__ == "__main__":
 
     # ----------------------
     # Aquí conectarse a MQTT
-
+    client = paho.Client("gps_mock_local")
+    client.on_connect = on_connect
+    client.connect(config["BROKER"], int(config["PORT"]))
+    client.loop_start()
 
     # ----------------------
     
@@ -28,6 +35,12 @@ if __name__ == "__main__":
 
     # ----------------------
     # Aquí preparar el blucle para enviar datos
-
-    
+    for i in range(20):
+        data["longitude"] +=.0001
+        json_str = json.dumps(data)
+        client.publish(topico,json_str)
+        time.sleep(2)
     # ----------------------
+    client.disconnect()
+    client.loop_stop()
+    # conectarle al MQTT Explore
